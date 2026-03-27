bfa8300911b67142c6636067e6ab227678590c65
snihlc <ineshalliche@gmail.com>
2026-03-27 02:26:56 +0100

    fix forms

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+function toggleSidebar(){
+                    document.getElementById("sidebar").classList.toggle("active");
+                    document.getElementById("sidebar-overlay").classList.toggle("active");
+                }
+
 function afficherChamps() {
 
     let type = document.getElementById("type-doc").value;
@@ -37,4 +42,36 @@ else {
         document.querySelector("#carte-grise").style.display="none";
 
 }
+}
+function affTypeMaintenance(){
+    let type = document.getElementById("type-maintenance").value;
+    let typeper = document.getElementById("type-periodique").value;
+
+    if(type == "periodique"){
+        document.querySelector(".mt-periodique").style.display = "block";
+        document.querySelector(".mt-corrective").style.display = "none";
+
+        if(typeper == "vidange"){
+            document.querySelector(".vidange").style.display = "block";
+            document.querySelector(".controle-frein").style.display = "none";
+        } else if(typeper == "controle-frein") {
+            document.querySelector(".controle-frein").style.display = "block";
+            document.querySelector(".vidange").style.display = "none";
+        } else {
+            document.querySelector(".vidange").style.display = "none";
+            document.querySelector(".controle-frein").style.display = "none";
+        }
+
+    } else if(type == "corrective") {
+        document.querySelector(".mt-corrective").style.display = "block";
+        document.querySelector(".mt-periodique").style.display = "none";
+        document.querySelector(".vidange").style.display = "none";
+        document.querySelector(".controle-frein").style.display = "none";
+
+    } else {
+        document.querySelector(".mt-periodique").style.display = "none";
+        document.querySelector(".mt-corrective").style.display = "none";
+        document.querySelector(".vidange").style.display = "none";
+        document.querySelector(".controle-frein").style.display = "none";
+    }
 }
